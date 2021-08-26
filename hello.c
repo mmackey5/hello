2bc1dc3475afb6cd76c4dafef669dc0c7df6babb
Michael Mackey <mmackey5@huskers.unl.edu>
2021-08-26 22:09:54 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Michael Mackey
+ * Date: 2021/8/26
+ *
+ * A simple hello world program
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Michael Mackey\n");
+
+    return 0;
+
+}
